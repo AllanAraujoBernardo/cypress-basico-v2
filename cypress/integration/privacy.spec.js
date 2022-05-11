59d9ac8bdc027d258d758f6ac3f239685080d4b3
Allan Araújo Bernardo <allan.bernando@bandtec.com.br>
2022-05-11 12:41:46 -0300

    Create cypress project

---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,6 @@
+it('testa a página de política de privaciade de forma independente', function() {
+    cy.visit('./src/privacy.html')
+    
+    cy.contains('Talking About Testing')
+        .should('be.visible')
+})
