ed1130eb760e53ea5b70307de7f4a3f34b824ea0
Allan Araújo Bernardo <allan.bernando@bandtec.com.br>
2022-05-12 11:02:32 -0300

    Finalização do Curso Básico

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,6 +1,8 @@
-it('testa a página de política de privaciade de forma independente', function() {
-    cy.visit('./src/privacy.html')
-    
-    cy.contains('Talking About Testing')
-        .should('be.visible')
+Cypress._.times(5,function() {
+    it('testa a página de política de privaciade de forma independente', function() {
+        cy.visit('./src/privacy.html')
+        
+        cy.contains('Talking About Testing')
+            .should('be.visible')
+    })
 })
